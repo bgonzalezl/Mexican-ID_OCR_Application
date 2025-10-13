724ae56030edbd59b3975d164851d598d49061c7
Bruno González <99365676+bgonzalezl@users.noreply.github.com>
2025-10-13 15:18:43 -0600

    Delete schema.py

--- schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-#Clase "ImageContainer" para definir el formato de las imágenes a recibir (base64)
-class ImageContainer(BaseModel):
-    front: str
-    back: str
-
-class TimeoutException(Exception): pass
